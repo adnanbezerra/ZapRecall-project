8dfb9d798661985f42f83b85e9d3dc342da3c41d
seu nome <adnanbezerra@gmail.com>
2022-11-10 16:08:26 -0300

    fix: remove unused component

--- src/components/InitialFooter.js
+++
@@ -1,8 +0,0 @@
-export default function InitialFooter({ quantity, done }) {
-    return (
-        <div className="footer">
-            <div className="text-content">{quantity}/8 CONCLUÍDAS </div>
-            <div className="done">{done}</div>
-        </div>
-    )
-}
